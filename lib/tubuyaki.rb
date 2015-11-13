aacf60680dffb43b542d1cd5557f180206527974
takkii <karuma.reason@gmail.com>
2015-11-13 23:15:56 +0900

    tweet button change img file.

--- lib/tubuyaki.rb
+++ lib/tubuyaki.rb
@@ -1,40 +1,5 @@
-#!/usr/bin/env ruby
-# -*- coding: utf-8 -*-
-# vim: filetype=ruby
+#  Copyright © 2015 Takayuki, Kamiyama
 
-lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
-module Tubuyaki
-
-def version
-require 'tubuyaki/version'
-print "Tubuyaki_Version : "
-puts Tubuyaki::VERSION
-end
-
-def start
-puts "取扱説明書".center(60,'-')
-puts ""
-puts "
-
->tubuyaki
-　∟tubuyakiを起動します
-
->tubuyaki -v
-　∟tubuyakiのバージョンを表示します
-
->tubuyaki -h
-　∟tubuyakiのヘルプを表示します
-
-tubuyakiはMITライセンスです
-
-"
-puts""
-puts "取扱説明書".center(60,'-')
-end
-
-def soft
 require 'tk'
 
 window = TkRoot.new{
@@ -47,12 +12,16 @@ title 'boyaki is shout!'
 }
     
 TkLabel.new(nil,
-:text => "ひとりごと、ぼやいったー！ポチっとな！",
+:text => "ひとりごと...顔だけキン肉マン...かおだけきん...",
 :fg => "blue").pack
+#家族、曰く私が眠くなるという独り言らしい
 
 entry = TkEntry.new.pack('side'=>'left')
 
-c = TkButton.new(:text => "(´･ω･｀)").pack('fill'=>'x',"padx"=>2.5,"pady"=>2.5)
+c = TkButton.new{
+  image TkPhotoImage.new("file" => "tweet_icon.gif")
+  pack('fill'=>'x',"padx"=>2.5,"pady"=>2.5)
+}
 
 c.command proc {
 
@@ -98,43 +67,3 @@ end
 }
 
 Tk.mainloop
-end
-
-module_function :soft, :version, :start
-end
-
-v = /\Aversion\z/
-ve = /\A[-][v]\z/
-h = /\Ahelp\z/
-he = /\A[-][h]\z/
-
-one = ARGV[0]
-
-case
-when one.nil?
-Tubuyaki.soft
-when one.match(v)
-Tubuyaki.version
-when one.match(ve)
-Tubuyaki.version
-when one.match(h)
-Tubuyaki.start
-when one.match(he)
-Tubuyaki.start
-else
-puts ''
-puts "Please choose an option."
-puts "Dear my wife."
-puts ''
-end
-
-# require "bundler/setup"
-# You can add fixtures and/or initialization code here to make experimenting
-# with your gem easier. You can also use a different console, if you like.
-
-# (If you use this, don't forget to add pry to your Gemfile!)
-# require "pry"
-# Pry.start
-
-# require "irb"
-# IRB.start
