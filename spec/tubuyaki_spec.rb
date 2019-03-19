4b61bdb52710f430630c57df535b2cc2252368fe
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-03-19 22:01:28 +0900

    Update.

--- spec/tubuyaki_spec.rb
+++ spec/tubuyaki_spec.rb
@@ -1,15 +1,16 @@
-#require 'spec_helper'
+# frozen_string_literal: true
 
 module Tubuyaki
-	module_function
-	def version
-		"0.0.3.1"
-	end
+  module_function
+
+  def version
+    '0.0.3.1'
+  end
 end
 
-describe "Tubuyaki" do
-	it ":expect say '0.0.3.1' when it receives the greet() message" do
-		versioning = Tubuyaki.version
-		expect(versioning) == "0.0.3.1"
-	end
+describe 'Tubuyaki' do
+  it ":expect say '0.0.3.1' when it receives the greet() message" do
+    versioning = Tubuyaki.version
+    expect(versioning) == '0.0.3.1'
+  end
 end
